f4bb220812516a4b57df641046b8651ec4394486
jenningsje <jenningsje@mac.com>
2022-06-03 13:14:22 -0500

    Protein-Cnfrm-Energy has bugs in it at the moment

--- Protein-Cnfrm-Energy.py
+++ Protein-Cnfrm-Energy.py
@@ -9,14 +9,9 @@ from gemmi import cif, CifWalk, expand_if_pdb_code
 import numpy as np
 from numpy import genfromtxt
 
-""" import the following files containing:
-(1) the list of amino acids
-(2) the table for the different types of combinations of hydrogen bonds between a pair of amino acids (20x20 matrix)
-(3) the table for the probability that a given hydrogen bond will form between two amino acids (20x20 matrix)
-(4) import the variables for the sidechain sidechain matrix 
-(5) import the functions necessary for constructing the """
+
 import sc_imports
-from obtain_monomer_coordinates import write_numpy_array
+from obtain_monomer_coordinates import names_and_coord
 
 # get the names of each protein as well as the monomer coordinates for them
-write_numpy_array()
+names_and_coord()
