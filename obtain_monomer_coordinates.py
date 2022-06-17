06f0d9373be42de6c307b972accb51567bf43e90
jenningsje <jenningsje@mac.com>
2022-06-17 12:38:27 -0500

    can convert byte string to int

--- obtain_monomer_coordinates.py
+++ obtain_monomer_coordinates.py
@@ -16,7 +16,7 @@ import forge_database
 from forge_database import get_file_paths_from_args
 from forge_database import write_numpy_array
 
-aa_dict = {"ALA": 1, "ARG": 2, "ASN": 3, "ASP": 4, "CYS": 5, "GLU": 6, "GLN": 7, "GLY": 8, "HIS": 9, "LIE": 10, "LEU": 11, "LYS": 12, "MET": 13, "PHE": 14, "PRO": 15, "SER": 16, "THR": 17, "TRP": 18, "TYR": 19, "VAL": 20}
+aa_dict = {"b'ALA'": 1, "b'ARG'": 2, "b'ASN'": 3, "b'ASP'": 4, "b'CYS'": 5, "b'GLU'": 6, "b'GLN'": 7, "b'GLY'": 8, "b'HIS'": 9, "b'LIE'": 10, "b'LEU'": 11, "b'LYS'": 12, "b'MET'": 13, "b'PHE'": 14, "bPRO": 15, "bSER": 16, "bTHR": 17, "bTRP": 18, "bTYR": 19, "b'VAL'": 20}
 
 atom_dict = {"C": 1, "N": 2, "O": 3, "ZN": 4}
 
@@ -45,7 +45,9 @@ def pipeline():
 
             # construct the block for the coordinate data below
             arr = genfromtxt('protein_coordinates.csv', delimiter=',', dtype=object)
+            print(arr)
+            prot_arr = []
 
             for i in range(1,len(arr)):
                 for j in range(3,5):
-                    print('success')
+                    print(float(arr[i][j]))
